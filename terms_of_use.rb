d9fefa4ffb7b487211386192887090c3f17e4a34
philiptang42 <philiptang42@gmail.com>
2015-12-06 21:24:38 -0500

    base disclaimer added

--- terms_of_use.rb
+++ terms_of_use.rb
@@ -1,1 +1,11 @@
+def disclaimer
+  puts "***DISCLAIMER***"
+  puts "This code may not work the way you expect!"
+  puts "By using this temperamental program, you agree not to"
+  puts "sue the pants off of its creator."
+  puts "***"
+  puts ""
+end
+
+disclaimer
 puts "3 + 2 = #{rand(6)}"
